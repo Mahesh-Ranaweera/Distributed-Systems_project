7c124c059d5fcb4a25bde4e8ce3b2282fca1198f
Travis Madill <TravisMadill@gmail.com>
2017-11-30 20:42:59 -0500

    Updated monitoring file

--- monitor.sh
+++ monitor.sh
@@ -1,10 +1,8 @@
 #! /bin/bash
-printf "CPU\t\tMemory\n"
+printf "CPU,Memory\n"
 end=$((SECONDS+3600))
 while [ $SECONDS -lt $end ]; do
-MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\t\t\n", $3*100/$2 }')
-DISK=$(df -h | awk '$NF=="/"{printf "%s\t\t", $5}')
-CPU=$(top -bn2 | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{printf "%.2f%%\n", 100 - $1}')
+MEMORY=$(free -m | awk 'NR==2{printf "%.2f%%\n", $3*100/$2 }')
+CPU=$(top -bn2 | awk '/^top -/ { p=!p } { if (!p) print }' | grep "Cpu(s)" | sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | awk '{printf "%.1f%%,", 100 - $1}')
 echo "$CPU$MEMORY"
-sleep .5
 done
